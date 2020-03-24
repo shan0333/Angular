a91c92511ccabc9b8a4a824eba68d330b2fae77f
shan0333 <48002153+shan0333@users.noreply.github.com>
2020-03-24 19:09:29 +0530

    Update login.component.ts

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -17,6 +17,11 @@ import { routerTransition } from '../../services/config/config.service';
 })
 export class LoginComponent implements OnInit {
 	loginForm: FormGroup;
+	cardValueCount:any; 
+	cardValue:any;
+	tempKey:any;
+	isClicked = false;
+	data:any=[{1:'50 SEK'}, {2:'100 SEK'}, {3:'500 SEK'}, {4:'1000 SEK'}, {5:'1500 SEK'}];
 	constructor(private formBuilder: FormBuilder, private router: Router, private userService: UserService, private toastr: ToastrService) {
 		this.loginForm = this.formBuilder.group({
 			email: ['', [Validators.required, ValidationService.emailValidator]],
@@ -26,15 +31,55 @@ export class LoginComponent implements OnInit {
 
 	// Check if user already logged in
 	ngOnInit() {
+		this.cardValueCount = 0
+	this.cardValue = '50 SEK'
+	
 		if (localStorage.getItem('userData')) {
 			this.router.navigate(['/']);
 		}
+		
 	}
 
 	// Initicate login
-	doLogin() {
-		const login = this.userService.doLogin(this.loginForm.value);
-		this.success(login);
+	// doLogin() {
+	// 	const login = this.userService.doLogin(this.loginForm.value);
+	// 	this.success(login);
+	// }
+
+	
+	onClick(value){
+		this.cardValue = value
+		var isClicked = true;
+	}
+
+	negative(key){
+		debugger
+		if (key != 0){
+			this.tempKey = key-2
+		key = key-1
+			console.log(this.data[this.tempKey][key])
+		this.cardValueCount = key
+		this.cardValue = this.data[this.tempKey][key]
+		}
+	
+	}
+	
+	positive(key){
+		debugger
+		if (key == 0){
+			this.tempKey = key+1
+		key = key  +2
+		}else{
+			this.tempKey = key
+			key = key  +1
+		}
+		
+		
+		
+		console.log(this.data[this.tempKey][key])
+		this.cardValueCount = key
+		this.cardValue = this.data[this.tempKey][key]
+
 	}
 
 	// Login success function
@@ -49,5 +94,4 @@ export class LoginComponent implements OnInit {
 	}
 
 }
-
 
