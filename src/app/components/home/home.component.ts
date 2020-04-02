a6413a29d27d4699373fe6b967aaa8aeeccb5d72
shan0333 <48002153+shan0333@users.noreply.github.com>
2020-04-02 16:48:39 +0530

    Update home.component.ts

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,7 @@
-/**
- * Created By : Sangwin Gawande (http://sangw.in)
- */
-
  import { Component, OnInit } from '@angular/core';
  import { RouterModule, Routes ,Router} from '@angular/router';
  import { ToastrService } from 'ngx-toastr';
 
- // Components
- import { StudentListComponent } from '../student/list/student-list.component';
- import { StudentDetailsComponent } from '../student/details/student-details.component';
- import { StudentAddComponent } from '../student/add/student-add.component';
 
  // Services
  import { routerTransition } from '../../services/config/config.service';
@@ -24,7 +16,8 @@
 
 
  export class HomeComponent implements OnInit {
- 	active:string;
+	 active:string;
+	 cardImg:any; 
  	constructor(private router: Router,private toastr: ToastrService) {
  		// Detect route changes for active sidebar menu
  		this.router.events.subscribe((val) => {
@@ -33,42 +26,24 @@
  	}
 
  	ngOnInit() {
- 	}
+		this.cardImg = "gCard3"
+		
+	}
+
+	
+	imageDynamic(value){
+		this.cardImg = value
+	}
+
+
 
  	// Detect route changes for active sidebar menu
  	routeChanged(val){
  		this.active = val.url;
  	}
 
- 	// Logout User
- 	logOut(){
- 		this.toastr.success('Success', "Logged Out Successfully");
- 		localStorage.removeItem('userData');
- 		this.router.navigate(['/login']);
- 	}
+ 	
  }
 
 
- // Define and export child routes of HomeComponent
- export const homeChildRoutes : Routes = [
- {
- 	path: '',
- 	component: StudentListComponent
- },
- {
- 	path: 'add',
- 	component: StudentAddComponent
- },
- {
- 	path: 'update/:id',
- 	component: StudentAddComponent
- },
- {
- 	path: 'detail/:id',
- 	component: StudentDetailsComponent
- }
- ];
-
-/**
- * Created By : Sangwin Gawande (http://sangw.in)
- */
+ 
