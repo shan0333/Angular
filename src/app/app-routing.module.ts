50075585632c463666321abf2d4ea55ec70067b1
shan0333 <48002153+shan0333@users.noreply.github.com>
2020-04-09 12:11:34 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
 import { AuthService } from './services/auth/auth.service';
-import { LoginComponent } from './components/login/login.component';
+
 import { CheckoutComponent } from './components/checkout/checkout.component';
 import { MailComponent } from './components/mail/mail.component';
 import { OrderhisComponent } from './components/orderhis/orderhis.component';
 import { FinalcheckoutComponent } from './components/finalcheckout/finalcheckout.component';
-
+import { GiftcardComponent } from './components/giftcard/giftcard.component';
+import { BuyComponent } from './components/buy/buy.component';
 
 
 // Parent Routes
 const routes: Routes = [
+  
   {
-    path: '',
-    component: HomeComponent,
-    canActivate: [AuthService]
-  },
-  {
-    path: 'orderhis',
-    component: OrderhisComponent
+    path: 'checkout',
+    component: BuyComponent
   },
   {
     path: '**',
