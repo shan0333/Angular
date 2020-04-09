17f310ca44e6040f971f966f766bd115c6af925f
shan0333 <48002153+shan0333@users.noreply.github.com>
2020-04-09 13:22:49 +0530

    Delete highlight-student.directive.spec.ts

--- src/app/directives/highlight-student.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HighlightStudentDirective } from './highlight-student.directive';
-
-describe('HighlightStudentDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HighlightStudentDirective();
-    expect(directive).toBeTruthy();
-  });
-});
