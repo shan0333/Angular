05569466c82be6c1fdb06cf71d2248dfceba4311
admin <admin@DESKTOP-98HRBAS>
2022-11-21 17:59:05 +0530

    PartLable Updated

--- spaceage/src/app/barcode/barcode.component.ts
+++ spaceage/src/app/barcode/barcode.component.ts
@@ -7,6 +7,7 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./barcode.component.css']
 })
 export class BarcodeComponent implements OnInit {
+	barcodeListFirst: any = [];
 	barcodeList: any = [];
 	partList: any = []; 
 	
@@ -14,7 +15,20 @@ export class BarcodeComponent implements OnInit {
 	
 	ngOnInit(): void {
 		this.partList.push(localStorage.getItem("list"));
-		this.barcodeList.push(JSON.parse(this.partList));
+
+		console.log(JSON.parse(this.partList));
+
+		this.barcodeListFirst.push(JSON.parse(this.partList));
+		this.barcodeListFirst[0].totalNoOfPackingGroup
+		if (this.barcodeListFirst[0].totalNoOfPackingGroup >1) {
+			for (let i = 0; i < this.barcodeListFirst[0].totalNoOfPackingGroup; i++) {	
+				this.barcodeList.push(JSON.parse(this.partList));
+				this.barcodeList[i].barCodeNo = this.barcodeList[i].partNo + (i+1) + this.barcodeListFirst[0].totalNoOfPackingGroup;
+				this.barcodeList[i].bomSlNo = i+1 + "-" + this.barcodeListFirst[0].totalNoOfPackingGroup;
+				console.log(this.barcodeList);
+
+			}
+		}
 		
   }
 
